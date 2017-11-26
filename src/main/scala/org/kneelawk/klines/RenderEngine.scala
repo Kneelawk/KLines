4e19599ec12bc92ad0909a5fd44b3960affd5af1
Kneelawk <jedidiah.pommert@gmail.com>
2017-11-25 19:52:29 -0800

    Thoughts on model rendering

--- src/main/scala/org/kneelawk/klines/RenderEngine.scala
+++ src/main/scala/org/kneelawk/klines/RenderEngine.scala
@@ -1,23 +1,28 @@
 package org.kneelawk.klines
 
+/*
+ * I wonder if you could have all the rendering code be in special
+ * implicit ModelRenderers and have each one know how to render a specific kind of model?
+ */
+
 trait RenderEngine {
   type UpdateCallback = () => Unit
-  
+
   /**
    * Initialize the engine.
    */
   def init(window: Window, camera: Camera)
-  
+
   /**
    * Start the engine's render loop.
    */
   def loop()
-  
+
   /**
    * Set the callback for every render cycle.
    */
   def setUpdateCallback(callback: UpdateCallback)
-  
+
   // TODO Some engines only work with some models
   def setModels()
 }
