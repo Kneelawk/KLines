3662c489652f9630dbe28bf5736c359ed660fbae
Kneelawk <jedidiah.pommert@gmail.com>
2019-04-12 10:30:57 -0700

    Begin working on the Model interface

--- src/main/scala/org/kneelawk/klines/Model.scala
+++ src/main/scala/org/kneelawk/klines/Model.scala
@@ -1,5 +1,12 @@
 package org.kneelawk.klines
 
-class Model {
+import org.joml.Matrix4f
+import org.kneelawk.klines.buffers.PartitionSystem
 
+trait Model {
+  def createPartitions(system: PartitionSystem)
+
+  def update()
+
+  def updatePartitions(transform: Matrix4f)
 }
